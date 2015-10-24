bcb12f5930afcee0e641da905908404fdd74ced7
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-10-23 21:10:52 -0500

    Change core module to integrate with node.js.

--- src/word_source/core.cljs
+++ src/word_source/core.cljs
@@ -1,9 +1,18 @@
 (ns word-source.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [cljs.nodejs :as nodejs])
+  (:use [clojure.string :only [split, split-lines]])
+  )
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
-(enable-console-print!)
+(def fs (js/require "fs"))
 
-(println "Hello world!")
+(def sprintf (js/require "sprintf-js"))
+
+(nodejs/enable-util-print!)
+
+(defn -main [& args]
+  (println "Hello world!"))
+
+(set! *main-cli-fn* -main)
